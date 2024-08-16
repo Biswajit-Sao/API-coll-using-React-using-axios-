d043f919df6ffa3012058d7a8edf123067da8a47
Biswajit-Sao <saobiswajit@gmail.com>
2024-08-15 22:56:36 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.css'
+import axios from 'axios';
+function App(){
+    const[a,b]=useState([]);
+    const[x,y]=useState([]);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const getImages=async()=>{
+        try{
+            const response =await axios.get("https://picsum.photos/v2/list");
+            const data=response.data;
+            console.log(data);
+            b(data);
+            console.log(a);
+
+        }catch(err){
+            console.error("sorry");
+        }
+    }
+
+    const getImages2=async()=>{
+        try{
+            const response =await axios.get("https://picsum.photos/v2/list");
+            const data=response.data;
+            console.log(data);
+            y(data);
+            console.log(a);
+
+        }catch(err){
+            console.error("sorry");
+        }
+    }
+
+    return(
+        <div>
+            <button onClick={getImages}>Enter</button>
+            <button onClick={getImages2}>Enter</button>
+
+            <div className='desig'>
+                {a.map((x)=>{
+                  return <img className='image-size' src={x.download_url} key={x}/>
+                })}
+            </div>
+
+            <div className='desig'>
+                {x.map((x)=>{
+                  return <img className='image-size' src={x.download_url} key={x}/>
+                })}
+            </div>
+        </div>
+    )
 }
-
 export default App;
+
